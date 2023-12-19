1b3813c9dd4b7570215e0792bcf61afc0b9f1a60
UnknownProoooLucky <138682002+Unknownproootest@users.noreply.github.com>
2023-12-19 22:17:45 +0800

    Update DexGui.lua

--- DexGui.lua
+++ DexGui.lua
@@ -1,12 +1,22 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Dark Dex Choosing", "DarkTheme")
 local Tab = Window:NewTab("Main")
-Section:NewButton("Dark Dex 1", "ButtonInfo", function()
+local Section = Tab:NewSection(" ")
+Section:NewButton("Dark Dex PC", "ButtonInfo", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownproootest/eof/main/DarkDex1'))()
    print("Clicked")
 end)
 
-Section:NewButton("Dark Dex 2", "ButtonInfo", function()
+Section:NewButton("Dark Dex Bypassed V3", "ButtonInfo", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Unknownproootest/eof/main/DarkDex2'))()
    print("Clicked")
+end)
+
+Section:NewButton("Dark Dex Mobile", "ButtonInfo", function()
+getgenv().Key = "Bash"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3AIY%20Dex",true))()
+end)
+
+Section:NewButton("Remote Spy", "ButtonInfo", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/Forked/M%3ASimpleSpy%20V3",true))()
 end)
